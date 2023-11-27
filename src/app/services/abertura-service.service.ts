775362664228aed0253588e814fd624d971ea1d1
NicolasPozzer <pozzernico@gmail.com>
2023-11-27 00:13:25 -0300

    deploy done2

--- src/app/services/abertura-service.service.ts
+++ src/app/services/abertura-service.service.ts
@@ -9,35 +9,35 @@ import { Abertura } from '../components/model/abertura';
 export class AberturaServiceService {
 
   //URL = 'http://localhost:8080/auth';
-  URL = 'https://jose-backend.onrender.com/auth';
+  URL = 'https://jose-backend.onrender.com/aberturas';
 
   constructor(private httpClient: HttpClient) { }
 
-  public lista(): Observable<Abertura[]>{
+  public lista(): Observable<Abertura[]> {
     return this.httpClient.get<Abertura[]>(this.URL + '/traertodos');
   }
 
-  public listaPuertas(): Observable<Abertura[]>{
+  public listaPuertas(): Observable<Abertura[]> {
     return this.httpClient.get<Abertura[]>(this.URL + '/puertas');
   }
 
-  public listaVentanas(): Observable<Abertura[]>{
+  public listaVentanas(): Observable<Abertura[]> {
     return this.httpClient.get<Abertura[]>(this.URL + '/ventanas');
   }
 
-  public detail(id: number): Observable<Abertura>{
+  public detail(id: number): Observable<Abertura> {
     return this.httpClient.get<Abertura>(this.URL + `/find/${id}`);
-  } 
+  }
 
-  public save(abertura: Abertura): Observable<any>{
+  public save(abertura: Abertura): Observable<any> {
     return this.httpClient.post<any>(this.URL + '/crear', abertura);
   }
 
-  public update(id: number, abertura: Abertura): Observable<any>{
+  public update(id: number, abertura: Abertura): Observable<any> {
     return this.httpClient.put<any>(this.URL + `/edit`, abertura);
   }
 
-  public delete(id: number): Observable<any>{
+  public delete(id: number): Observable<any> {
     return this.httpClient.delete<any>(this.URL + `/borrar/${id}`);
   }
 }
